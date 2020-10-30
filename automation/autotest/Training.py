9c404639184042101a006b5dce61be8bcad6ea6e
George Joseph <thegeorgejoseph@gmail.com>
2020-10-31 01:59:56 +0530

    Automation with Docker

---
+++ automation/autotest/Training.py
@@ -0,0 +1,103 @@
+from userInputs import *
+from INIT import *
+from score import *
+from all_other_functions import targetAnalysis
+import time
+import pandas as pd
+import numpy as np
+import swifter
+import joblib
+import warnings
+warnings.filterwarnings('ignore')
+import matplotlib.pyplot as plt
+import re
+from sklearn.model_selection import train_test_split
+from engineerings import numeric_engineering
+
+def main(test=False,Path=None):
+    '''
+    PROTON MAIN FUNCTION
+    '''
+    spinnerBool = False
+    if not test:path = input('Enter the path here : ')
+    else:path = Path
+    error = False
+    if path:
+        df,csvPath = importFile(path,nrows=30)
+        df = df.rename(columns = lambda x:re.sub('[^A-Za-z0-9_]+', '', x))
+        df = dataHandler(df) # If first few rows contains unnecessary info
+        info = getUserInput(df,test=test)
+        if not info:
+            error = True
+    else:
+        df = None
+        print('\nQuitting Process\n')
+        info = None
+        error = True
+
+    te = time.time()
+    try:
+        if info:
+            spinnerBool = True
+            ################## TRAINING INIT ##################
+            if csvPath:
+                path = 'SheetSheetSheet.csv'
+            df,_ = importFile(path,nrows=None)
+            df = df.rename(columns = lambda x:re.sub('[^A-Za-z0-9_]+', '', x))
+            df = dataHandler(df) # If first few rows contains unnecessary info
+            tts = time.time()
+            if isinstance(df,pd.DataFrame):
+                target = info['target']
+                print("###Performing Initial Numeric Engineering for Capping Purposes###")
+                dfsamp = df.sample(n=1000,random_state=1) if len(df)>1000 else df.copy()
+                dfsamp = numeric_engineering(dfsamp)
+                dfsamp = dfsamp.dropna(axis=0,subset=[target])
+                class_or_Reg = targetAnalysis(dfsamp[target])                    
+                if class_or_Reg == 'Classification':
+                    if len(df) >1000000:
+                        df_train, _ = train_test_split(df, train_size=1000000,random_state=1, stratify=df[target])
+                        print("Dataset size has been capped to 10 lakh rows for better performance")
+                        print("Length of the dataset is now",len(df_train))
+                        init_info,validation = INIT(df_train,info)
+                    else:
+                        print("Dataset has not been capped")
+                        print("Length of the dataset is same as original",len(df))
+                        init_info,validation = INIT(df,info)
+                elif class_or_Reg == 'Regression':
+                    dfr = df.sample(n=1000000, random_state=1) if len(df)>1000000 else df.copy()
+                    print("Dataset size has been capped to 10 lakh rows for better performance")
+                    print("Length of the dataset is now",len(dfr))
+                    init_info,validation = INIT(dfr,info)
+                elif class_or_Reg is None:
+                    init_info,validation = None,None
+            else:
+                init_info,validation = None,None
+            tte = time.time()
+            print('\n TOTAL TRAINING DATA CLEANING AND PLOTS : {}'.format(tte-tts))
+            ################## TRAINING INIT ##################
+
+            if isinstance(validation,pd.DataFrame):
+                ################## VALIDATION AND PREDICTION ##################
+                score(validation,init_info,validation=True)
+                ################## VALIDATION AND PREDICTION ##################
+                print('\n\t #### CODE EXECUTED SUCCESSFULLY ####')
+                print('\n\t #### END ####')
+            else:
+                print('\n\t #### CODE DID NOT RUN COMPLETELY ####')
+            spinnerBool = False
+    except KeyboardInterrupt:
+        print('QUITTING!')   
+        return None
+    # except Exception as e:
+    #     print('Code did not run completely')
+    #     print('Code ran into an error')
+    #     print('The error message received is')
+    #     print(e)
+    #     return None
+    ee = time.time()
+    print('\n#### TOTAL TIME TAKEN : {} ####'.format(ee-te))
+    return 1
+
+if __name__ == '__main__':
+    # Main Function call
+    ret = main()
