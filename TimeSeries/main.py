345274c264b87185d66dc1a3bf78d74ea7ecdb27
Ganesh Ram Gururajan <42694700+ganeshramg@users.noreply.github.com>
2020-11-04 14:53:09 +0530

    Create main.py
    For Time Series

---
+++ TimeSeries/main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+def main():
+    print('This is Time Series Folder and All functions and files will be contained here')
+
+
+if __name__ == '__main__':
+    main()
