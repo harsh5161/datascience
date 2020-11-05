f182b0e8a9fc813da037e22d836e344a938e5370
Ganesh Ram Gururajan <42694700+ganeshramg@users.noreply.github.com>
2020-11-05 16:43:43 +0530

    Basic Structure

--- TimeSeries/init.py
+++ TimeSeries/init.py
@@ -1,11 +1,5 @@
 from userInputs import importFile
 from engineerings import numeric_engineering,date_engineering
 
-def TimeSeriesInit(path,info):
-    df,_ = importFile(path)
-
-    # Numeric Engineering
-    df = numeric_engineering(df)
-
-    return None,None
-
+def INIT(df,info):
+    pass
--- TimeSeries/main.py
+++ TimeSeries/main.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from userInputs import importFile,getInfo
 from engineerings import getDateColumns
-from init import TimeSeriesInit
+from init import Table
 
 def main(test=False,props=None):
     print('This is Time Series Folder and All functions and files will be contained here')
@@ -17,7 +17,7 @@ def main(test=False,props=None):
         df,_ = importFile('./test/' + props[path],nrows=100)
     # Stripping trailing and leading spaces and replacing spaces in between with _
     df.columns = [x.strip().replace(' ','_') for x in df.columns]
-    datecols = getDateColumns(df)
+    datecols = getDateColumns(df.select_dtypes('object'))
     if len(datecols) == 0:
         print('No datecolumns found, QUITTING!')
         return None,None
@@ -27,7 +27,7 @@ def main(test=False,props=None):
         print('QUITTING!')
         return None,None
     else:
-        TimeSeriesInit('./test/' + path,info)
+        validation,init_info = INIT(df,info)
 
 if __name__ == '__main__':
     returnValue,numberOfErrors = main()
