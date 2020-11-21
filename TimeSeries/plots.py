be7a77d82ea875688c939122798f983411f6d6e9
Ganesh Ram Gururajan <42694700+ganeshramg@users.noreply.github.com>
2020-11-21 16:04:39 +0530

    Update plots.py

--- TimeSeries/plots.py
+++ TimeSeries/plots.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 
 def basicPlot(props):
+    # This will be replaced with PlotLy.JS
     df = props['df']
     info = props['info']
     plt.plot(df[info['Target']])
