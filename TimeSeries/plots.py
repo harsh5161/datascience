c6d43261582164d33b86a931f0827c0deebc9711
Ganesh Ram Gururajan <42694700+ganeshramg@users.noreply.github.com>
2020-11-26 18:24:47 +0530

    Decomposition Plot Update

--- TimeSeries/plots.py
+++ TimeSeries/plots.py
@@ -1,9 +1,21 @@
 import matplotlib.pyplot as plt
+from statsmodels.tsa.seasonal import seasonal_decompose
 
 def basicPlot(props):
     # This will be replaced with PlotLy.JS
+    print('\nPlotting Basic Target without any changes!')
     df = props['df']
     df.index = df[props['info']['PrimaryDate']]
     info = props['info']
     plt.plot(df[info['Target']])
     return None
+
+def decompositionPlot(props):
+    df = props['df']
+    df.index = df[props['info']['PrimaryDate']]
+    target = props['info']['Target']
+    resultPlot = seasonal_decompose(df[target],model='multiplicative')
+    resultPlot.plot()
+    plt.show()
+    
+        
