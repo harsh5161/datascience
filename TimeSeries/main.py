c93782419978b340abbf6c23109d9a9047e0e8da
Ganesh Ram Gururajan <42694700+ganeshramg@users.noreply.github.com>
2020-12-04 19:49:59 +0530

    GNB

--- TimeSeries/main.py
+++ TimeSeries/main.py
@@ -9,12 +9,16 @@ from plots import basicPlot,decompositionPlot,fbprophet_plots,neural_prophet_plo
 from init import INIT
 import joblib
 from pmdarima import auto_arima
+from pmdarima.arima import ndiffs
 from pmdarima.metrics import smape
 from sklearn.metrics import mean_squared_error,mean_absolute_error
+from sklearn.naive_bayes import GaussianNB
 import matplotlib.pyplot as plt
+plt.style.use('Solarize_Light2')
 from fbprophet import Prophet
 from neuralprophet import NeuralProphet
 import holidays
+import pdb
 
 def main(test=False,props=None):
     print('This is Time Series Folder and All functions and files will be contained here')
@@ -118,68 +122,105 @@ def main(test=False,props=None):
 # =============================================================================
 
 # =============================================================================
-# FBPROPHET
+# # =============================================================================
+# # FBPROPHET
+# # =============================================================================
+# 
+#     print('\n#### FBPROPHET MODEL #### RUNNING WAIT ####')
+#     fbprophetModel = Prophet()
+#     dsy = pd.concat([X_train,y_train.rename('y')],axis=1)
+#     dsy['ds'] = dsy.index
+#     for col in X_train.columns:
+#         fbprophetModel.add_regressor(col)    
+#     future = X_test
+#     future['ds'] = X_test.index
+#     fbProphetForecasts = fbprophetModel.fit(dsy).predict(future)
+#     fbProphetForecasts.index = y_test.index
+#     print('\nThe mean squared error for FBProphet is') 
+#     print(mean_squared_error(y_test,fbProphetForecasts['yhat']))
+#     props['FBProphet'] = fbprophetModel
+#     MODEL_COMPARISON.loc[mc_cols_index,'Model'] = fbprophetModel
+#     MODEL_COMPARISON.loc[mc_cols_index,'Model Name'] = 'Facebook Prophet'
+#     MODEL_COMPARISON.loc[mc_cols_index,'Mean Absolute Percentage Error'] = smape(y_test, fbProphetForecasts['yhat'])
+#     MODEL_COMPARISON.loc[mc_cols_index,'Mean Squared Error'] = mean_squared_error(y_test,fbProphetForecasts['yhat'])
+#     MODEL_COMPARISON.loc[mc_cols_index,'Mean Absolute Error'] = mean_absolute_error(y_test,fbProphetForecasts['yhat'])
+#     plt.plot(y_test,label='Actual Values')
+#     plt.plot(fbProphetForecasts['yhat'],label='FBProphet Prediction')
+#     plt.legend(loc=2)
+#     plt.show()
+#     fbprophet_plots(fbprophetModel,fbProphetForecasts)
+#     mc_cols_index += 1
+# 
+# # =============================================================================
+# # FBPROPHET
+# # =============================================================================
+# 
+# # =============================================================================
+# # NEURALPROPHET
+# # =============================================================================
+# 
+#     print('\n#### NEURAL PROPHET MODEL #### RUNNING WAIT ####')
+#     neuralProphetModel = NeuralProphet(learning_rate=0.01,epochs=500,n_forecasts=10,loss_func='mse')
+#     for col in X_train.columns:
+#         neuralProphetModel.add_future_regressor(col)  
+#     epoch_df_info = neuralProphetModel.fit(dsy,freq='D')
+#     future = neuralProphetModel.make_future_dataframe(dsy,periods=len(y_test),regressors_df=X_test)
+#     future.index = X_test.index
+#     neuralProphetForecasts = neuralProphetModel.predict(future)
+#     neuralProphetForecasts.index = y_test.index
+#     print('\nThe mean squared error for Neural Prophet is') 
+#     print(mean_squared_error(y_test,neuralProphetForecasts['yhat1']))
+#     props['NeuralProphet'] = neuralProphetModel
+#     MODEL_COMPARISON.loc[mc_cols_index,'Model'] = neuralProphetModel
+#     MODEL_COMPARISON.loc[mc_cols_index,'Model Name'] = 'Neural Prophet'
+#     MODEL_COMPARISON.loc[mc_cols_index,'Mean Absolute Percentage Error'] = smape(y_test, neuralProphetForecasts['yhat1'])
+#     MODEL_COMPARISON.loc[mc_cols_index,'Mean Squared Error'] = mean_squared_error(y_test,neuralProphetForecasts['yhat1'])
+#     MODEL_COMPARISON.loc[mc_cols_index,'Mean Absolute Error'] = mean_absolute_error(y_test,neuralProphetForecasts['yhat1'])
+#     plt.plot(y_test,label='Actual Values')
+#     plt.plot(neuralProphetForecasts['yhat1'],label='Neural Prophet Prediction')
+#     plt.legend(loc=2)
+#     plt.show()
+#     neural_prophet_plots(neuralProphetModel,neuralProphetForecasts)
+#     mc_cols_index += 1
+#     
+# # =============================================================================
+# # NEURALPROPHET
+# # =============================================================================
 # =============================================================================
 
-    print('\n#### FBPROPHET MODEL #### RUNNING WAIT ####')
-    fbprophetModel = Prophet()
-    dsy = pd.concat([X_train,y_train.rename('y')],axis=1)
-    dsy['ds'] = dsy.index
-    for col in X_train.columns:
-        fbprophetModel.add_regressor(col)    
-    future = X_test
-    future['ds'] = X_test.index
-    fbProphetForecasts = fbprophetModel.fit(dsy).predict(future)
-    fbProphetForecasts.index = y_test.index
-    print('\nThe mean squared error for FBProphet is') 
-    print(mean_squared_error(y_test,fbProphetForecasts['yhat']))
-    props['FBProphet'] = fbprophetModel
-    MODEL_COMPARISON.loc[mc_cols_index,'Model'] = fbprophetModel
-    MODEL_COMPARISON.loc[mc_cols_index,'Model Name'] = 'Facebook Prophet'
-    MODEL_COMPARISON.loc[mc_cols_index,'Mean Absolute Percentage Error'] = smape(y_test, fbProphetForecasts['yhat'])
-    MODEL_COMPARISON.loc[mc_cols_index,'Mean Squared Error'] = mean_squared_error(y_test,fbProphetForecasts['yhat'])
-    MODEL_COMPARISON.loc[mc_cols_index,'Mean Absolute Error'] = mean_absolute_error(y_test,fbProphetForecasts['yhat'])
-    plt.plot(y_test,label='Actual Values')
-    plt.plot(fbProphetForecasts['yhat'],label='FBProphet Prediction')
-    plt.legend(loc=2)
-    plt.show()
-    fbprophet_plots(fbprophetModel,fbProphetForecasts)
-    mc_cols_index += 1
-
 # =============================================================================
-# FBPROPHET
+# NAIVE BAYES
 # =============================================================================
 
-# =============================================================================
-# NEURALPROPHET
-# =============================================================================
-
-    print('\n#### NEURAL PROPHET MODEL #### RUNNING WAIT ####')
-    neuralProphetModel = NeuralProphet(learning_rate=0.01,epochs=500,n_forecasts=10,loss_func='mse')
-    for col in X_train.columns:
-        neuralProphetModel.add_future_regressor(col)  
-    epoch_df_info = neuralProphetModel.fit(dsy,freq='D')
-    future = neuralProphetModel.make_future_dataframe(dsy,periods=len(y_test),regressors_df=X_test)
-    future.index = X_test.index
-    neuralProphetForecasts = neuralProphetModel.predict(future)
-    neuralProphetForecasts.index = y_test.index
-    print('\nThe mean squared error for Neural Prophet is') 
-    print(mean_squared_error(y_test,neuralProphetForecasts['yhat1']))
-    props['NeuralProphet'] = neuralProphetModel
-    MODEL_COMPARISON.loc[mc_cols_index,'Model'] = neuralProphetModel
-    MODEL_COMPARISON.loc[mc_cols_index,'Model Name'] = 'Neural Prophet'
-    MODEL_COMPARISON.loc[mc_cols_index,'Mean Absolute Percentage Error'] = smape(y_test, neuralProphetForecasts['yhat1'])
-    MODEL_COMPARISON.loc[mc_cols_index,'Mean Squared Error'] = mean_squared_error(y_test,neuralProphetForecasts['yhat1'])
-    MODEL_COMPARISON.loc[mc_cols_index,'Mean Absolute Error'] = mean_absolute_error(y_test,neuralProphetForecasts['yhat1'])
-    plt.plot(y_test,label='Actual Values')
-    plt.plot(neuralProphetForecasts['yhat1'],label='Neural Prophet Prediction')
-    plt.legend(loc=2)
-    plt.show()
-    neural_prophet_plots(neuralProphetModel,neuralProphetForecasts)
-    mc_cols_index += 1
+    pdb.set_trace()
     
+    print('\n#### GAUSSIAN NAIVE BAYES MODEL #### RUNNING WAIT ####')
+    naiveBayesModel = GaussianNB()
+    print('\nEstimating difference term d using AutoArima - ADF test(without exogenous variables)!')
+    d_term = ndiffs(x=y_train,test='adf') 
+    print('\nThe d_term found for stationarity is {}'.format(d_term))
+    print('\nDifferencing Time Series using d_term!')
+    y_train_diff = pd.Series(y_train - y_train.shift(d_term),index=y_train.index)
+    drop_indices = y_train[y_train.isnull()].index
+    X_train_gnb = X_train.drop(drop_indices)
+    naiveBayesModel.fit(X_train_gnb,y_train_diff)
+    NaiveBayesPredictions = pd.Series(naiveBayesModel.predict(X_test),index=y_test.index)
+    print('\nThe mean squared error for Gaussian Naive Bayes is')
+    print(mean_squared_error(y_test, NaiveBayesPredictions))
+    props['NaiveBayes'] = naiveBayesModel
+    MODEL_COMPARISON.loc[mc_cols_index,'Model'] = naiveBayesModel
+    MODEL_COMPARISON.loc[mc_cols_index,'Model Name'] = 'Gaussian Naive Bayes'
+    MODEL_COMPARISON.loc[mc_cols_index,'Mean Absolute Percentage Error'] = smape(y_test, NaiveBayesPredictions)
+    MODEL_COMPARISON.loc[mc_cols_index,'Mean Squared Error'] = mean_squared_error(y_test, NaiveBayesPredictions)
+    MODEL_COMPARISON.loc[mc_cols_index,'Mean Absolute Error'] = mean_absolute_error(y_test, NaiveBayesPredictions)
+    plt.plot(y_test,label='Actual Values')
+    plt.plot(NaiveBayesPredictions,label='Gaussian Naive Bayes Prediction')
+    plt.legend(loc=2)
+    plt.show()
+    mc_cols_index += 1    
+
 # =============================================================================
-# NEURALPROPHET
+# NAIVE BAYES
 # =============================================================================
 
     MODEL_COMPARISON.drop(['Model'],axis=1).to_csv('MC.csv')
