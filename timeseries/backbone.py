5e9a0640f99ce6f6c282863344f1e0aeff11bcaf
George Joseph <thegeorgejoseph@gmail.com>
2021-05-11 14:18:31 +0530

    setting up the architecture for different timeseries analysis

--- timeseries/backbone.py
+++ timeseries/backbone.py
@@ -12,3 +12,25 @@ def INIT():
 
     if len(action_list) == 0:
         return None
+
+    resampled_frames = dataResampler(df, action_list)
+
+    for val in action_list:
+        if val == 'Y':
+            # call function passing df,target and other parameters specific to Y
+            print("function Y")
+        if val == 'M':
+            # call function passing df,target and other parameters specific to M
+            print("function M")
+        if val == 'D':
+            # call function passing df,target and other parameters specific to D
+            print("function D")
+        if val == 'RQ':
+            # call function passing resampled_frames['Quarter'],target and other parameters specific to RQ
+            print("function RQ")
+        if val == 'RM':
+            # call function passing resampled_frames['Monthly'],target and other parameters specific to RM
+            print("function RM")
+        if val == 'RW':
+            # call function passing resampled_frames['Daily'],target and other parameters specific to RW
+            print("function RW")
