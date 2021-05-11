ec1e850ceb024357404032f38265c6479a9194c7
George Joseph <thegeorgejoseph@gmail.com>
2021-05-11 13:23:13 +0530

    Creating run.py and setting symlinks

--- timeseries/backbone.py
+++ timeseries/backbone.py
@@ -1,2 +1,4 @@
 # Central Pipeline where Data will be passed around
 
+def INIT():
+    sd
---
+++ timeseries/run.py
@@ -0,0 +1,63 @@
+# Adding Imports
+# File Imports
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from bayes_opt import BayesianOptimization
+from tqdm import tqdm, tqdm_notebook
+from pylab import rcParams
+from itertools import islice
+from sklearn.model_selection import cross_val_score, GridSearchCV
+from sklearn.metrics import mean_squared_error, mean_absolute_error, make_scorer
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn import linear_model, svm
+from statsmodels.tsa.holtwinters import SimpleExpSmoothing, ExponentialSmoothing
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.seasonal import seasonal_decompose
+from datetime import datetime
+from random import random
+from statsmodels.tsa.ar_model import AR
+from userInputs import *
+from backbone import *
+
+# Library Imports
+import time
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import pandas as pd  # Basic library for all of our dataset operations
+import numpy as np
+import requests
+import io
+import statsmodels.tsa.api as smt
+import statsmodels as sm
+import pmdarima as pm
+import warnings
+import xgboost as xgb
+from math import sqrt
+from metrics import evaluate
+from plots import bar_metrics
+
+warnings.filterwarnings("ignore")
+
+
+# from fbprophet import Prophet
+
+
+# Extra settings
+seed = 42
+# tf.random.set_seed(seed)
+np.random.seed(seed)
+plt.style.use('bmh')
+mpl.rcParams['axes.labelsize'] = 14
+mpl.rcParams['xtick.labelsize'] = 12
+mpl.rcParams['ytick.labelsize'] = 12
+mpl.rcParams['text.color'] = 'k'
+# print(tf.__version__)
+
+try:
+    start = time.time()
+    INIT()
+    print(f"Total Time taken for successful completion : {time.time()-start}")
+except Exception as e:
+    print(f"The program had errors : {e}")
